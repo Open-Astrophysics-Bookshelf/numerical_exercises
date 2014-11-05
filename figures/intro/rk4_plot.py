5557133cb538452563fbca3d3e340259b7eb26a1
Michael Zingale <michael.zingale@stonybrook.edu>
2014-11-04 21:10:42 -0500

    fix up the RK4 figure

--- figures/intro/rk4_plot.py
+++ figures/intro/rk4_plot.py
@@ -82,31 +82,22 @@ ltext = leg.get_texts()
 pylab.setp(ltext, fontsize='large')
 leg.draw_frame(0)
 
-pylab.xlim(t0-0.05*dt, t0+2.1*dt)
+pylab.xlim(t0-0.25*dt, t0+2.1*dt)
 
 pylab.tight_layout()
 pylab.savefig("rk4_Euler.eps", bbox_inches='tight')
 
 
-
-# k1 slope uses y^n position
+#----------------------------------------------------------------------------
+# show k1 slope 
 pylab.clf()
 start()
 
 pylab.axis("off")
 
 k1 = rhs(y0)
-thalf = numpy.linspace(t0, t0+0.5*dt, 2)
-yEuler = y0 + k1*(thalf-t0)
-
-pylab.plot(thalf, yEuler, label="half-dt k1 step", color="b")
-
-pylab.scatter([thalf[1]], [yEuler[1]], color="b")
-
-# illustrate the slope at that position
-k2 = rhs(yEuler[1])
-tSlope = numpy.linspace(thalf[1]-0.2*dt, thalf[1]+0.2*dt, 2)
-ySlope = yEuler[1] + k2*(tSlope - thalf[1])
+tSlope = numpy.linspace(t0-0.2*dt, t0+0.2*dt, 2)
+ySlope = y0 + k1*(tSlope - t0)
 
 pylab.plot(tSlope, ySlope, label=r"slope", color="g", ls="--", lw=2)
 
@@ -115,13 +106,42 @@ ltext = leg.get_texts()
 pylab.setp(ltext, fontsize='large')
 leg.draw_frame(0)
 
-pylab.xlim(t0-0.05*dt, t0+2.1*dt)
+pylab.xlim(t0-0.25*dt, t0+2.1*dt)
 
 pylab.tight_layout()
 pylab.savefig("rk4_k1.eps", bbox_inches='tight')
 
 
-# k2 slope goes from y^n to 1/2 time to get k3 slope
+#----------------------------------------------------------------------------
+# follow k1 to define k2
+
+ytmp = y0 + k1*0.5*dt
+k2 = rhs(ytmp)
+
+# draw the k1 half step
+pylab.plot([t0, t0+0.5*dt], [y0, ytmp], color="b", label="half-dt k1 step")
+pylab.scatter(t0+0.5*dt, ytmp, color="b")
+
+# draw slope there
+
+tSlope = numpy.linspace(t0+0.5*dt-0.2*dt, t0+0.5*dt+0.2*dt, 2)
+ySlope = ytmp + k2*(tSlope - (t0 + 0.5*dt))
+
+pylab.plot(tSlope, ySlope, color="g", ls="--", lw=2)
+
+leg = pylab.legend()
+ltext = leg.get_texts()
+pylab.setp(ltext, fontsize='large')
+leg.draw_frame(0)
+
+pylab.xlim(t0-0.25*dt, t0+2.1*dt)
+
+pylab.tight_layout()
+pylab.savefig("rk4_k2.eps", bbox_inches='tight')
+
+
+#----------------------------------------------------------------------------
+# follow k2 to define k3
 ytmp = y0 + k2*0.5*dt
 k3 = rhs(ytmp)
 
@@ -139,13 +159,14 @@ ltext = leg.get_texts()
 pylab.setp(ltext, fontsize='large')
 leg.draw_frame(0)
 
-pylab.xlim(t0-0.05*dt, t0+2.1*dt)
+pylab.xlim(t0-0.25*dt, t0+2.1*dt)
 
 pylab.tight_layout()
-pylab.savefig("rk4_k2.eps", bbox_inches='tight')
+pylab.savefig("rk4_k3.eps", bbox_inches='tight')
 
 
-# k3 slope goes from y^n to full time to get k4 slope
+#----------------------------------------------------------------------------
+# follow k3 to define k4
 ytmp = y0 + k3*dt
 k4 = rhs(ytmp)
 
@@ -164,19 +185,20 @@ ltext = leg.get_texts()
 pylab.setp(ltext, fontsize='large')
 leg.draw_frame(0)
 
-pylab.xlim(t0-0.05*dt, t0+2.1*dt)
+pylab.xlim(t0-0.25*dt, t0+2.1*dt)
 
 pylab.tight_layout()
-pylab.savefig("rk4_k3.eps", bbox_inches='tight')
+pylab.savefig("rk4_k4.eps", bbox_inches='tight')
 
 
 
+#----------------------------------------------------------------------------
 # final RK-4 step
 ynew = y0 + (dt/6.0)*(k1 + 2.0*k2 + 2.0*k3 + k4)
 
 
 # draw full RK-4 step
-pylab.plot([t0, t0+dt], [y0, ynew], color="r", label="full-dt R-K 4 step")
+pylab.plot([t0, t0+dt], [y0, ynew], color="r", label="full 4th-order RK step")
 pylab.scatter(t0+dt, ynew, color="r")
 pylab.text(t0+1.05*dt, ynew+0.015, r"$y^{n+1}$", color = "r", 
            horizontalalignment="left", fontsize=20)
@@ -187,7 +209,7 @@ ltext = leg.get_texts()
 pylab.setp(ltext, fontsize='large')
 leg.draw_frame(0)
 
-pylab.xlim(t0-0.05*dt, t0+2.1*dt)
+pylab.xlim(t0-0.25*dt, t0+2.1*dt)
 
 pylab.tight_layout()
 pylab.savefig("rk4_final.eps", bbox_inches='tight')
