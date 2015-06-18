b1f4a26e25b74fd7b943d3ec154035149fe14ddc
Michael Zingale <michael.zingale@stonybrook.edu>
2015-06-18 16:02:41 -0400

    add a dx scale

--- figures/intro/derivatives.py
+++ figures/intro/derivatives.py
@@ -1,39 +1,60 @@
-import numpy
-import pylab
-
-x_smooth = numpy.linspace(0.0, numpy.pi, 500)
-f_smooth = numpy.sin(x_smooth)
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
 
 
-x = numpy.linspace(0.0, numpy.pi, 10)
-f = numpy.sin(x)
+x_smooth = np.linspace(0.0, np.pi, 500)
+f_smooth = np.sin(x_smooth)
+
+
+x = np.linspace(0.0, np.pi, 10)
+f = np.sin(x)
 
 i = 5
 x_0 = x[i]
 f_0 = f[i]
 
-pylab.plot(x_smooth, f_smooth)
-pylab.scatter(x, f)
+plt.plot(x_smooth, f_smooth)
+plt.scatter(x, f)
 
-pylab.scatter(x_0, numpy.sin(x_0), color="r", zorder=10)
+plt.scatter(x_0, np.sin(x_0), color="r", zorder=10)
 
 dx = x[1] - x[0]
-x_d = numpy.linspace(x[i]-1.5*dx, x[i]+1.5*dx, 2)
+x_d = np.linspace(x[i]-1.5*dx, x[i]+1.5*dx, 2)
 
-d_exact = numpy.cos(x_0)
+d_exact = np.cos(x_0)
 
-d_l = (numpy.sin(x[i]) - numpy.sin(x[i-1]))/dx
-d_r = (numpy.sin(x[i+1]) - numpy.sin(x[i]))/dx
-d_c = 0.5*(numpy.sin(x[i+1]) - numpy.sin(x[i-1]))/dx
+d_l = (np.sin(x[i]) - np.sin(x[i-1]))/dx
+d_r = (np.sin(x[i+1]) - np.sin(x[i]))/dx
+d_c = 0.5*(np.sin(x[i+1]) - np.sin(x[i-1]))/dx
 
 
-pylab.plot(x_d, d_exact*(x_d - x_0) + f_0, color="0.5", lw=2, ls=":", label="exact")
-pylab.plot(x_d, d_l*(x_d - x_0) + f_0, label="left-sided")
-pylab.plot(x_d, d_r*(x_d - x_0) + f_0, label="right-sided")
-pylab.plot(x_d, d_c*(x_d - x_0) + f_0, label="centered")
+# plot a line showing dx
+dx = x[2] - x[1]
+ann = plt.annotate('', xy=(x[1], f[1]-0.5*dx),  xycoords='data',
+                   xytext=(x[2], f[1]-0.5*dx), textcoords='data',
+                   arrowprops=dict(arrowstyle="<->",
+                                   connectionstyle="bar,fraction=-0.4",
+                                   ec="k",
+                                   shrinkA=5, shrinkB=5,
+                   ))
 
-ax = pylab.gca()
+plt.text(0.5*(x[1]+x[2]), f[1]-0.8*dx, "$\Delta x$",
+         horizontalalignment="center")
 
+plt.plot([x[1], x[1]], [f[1]-0.5*dx, f[2]+0.5*dx], ls=":", color="0.5")
+plt.plot([x[2], x[2]], [f[1]-0.5*dx, f[2]+0.5*dx], ls=":", color="0.5")
+
+plt.plot(x_d, d_exact*(x_d - x_0) + f_0, color="0.5", lw=2, ls=":", label="exact")
+plt.plot(x_d, d_l*(x_d - x_0) + f_0, label="left-sided")
+plt.plot(x_d, d_r*(x_d - x_0) + f_0, label="right-sided")
+plt.plot(x_d, d_c*(x_d - x_0) + f_0, label="centered")
+
+
+ax = plt.gca()
+
+
+# origin of the axes through 0
 ax.spines['left'].set_position('zero')
 ax.spines['right'].set_color('none')
 ax.spines['bottom'].set_position('zero')
@@ -43,13 +64,14 @@ ax.spines['bottom'].set_smart_bounds(True)
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
 
-pylab.legend(frameon=False, loc="best")
+plt.legend(frameon=False, loc="best")
 
-pylab.ylim(-0.1, 1.1)
+plt.ylim(-0.1, 1.1)
 
-pylab.tight_layout()
+#plt.tight_layout()
 
-pylab.savefig("derivs.eps")
+
+plt.savefig("derivs.eps")
 
 
 
