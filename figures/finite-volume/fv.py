bcc0c7cf6f6bb44fa90b26eac6c446547245528f
Michael Zingale <michael.zingale@stonybrook.edu>
2015-08-28 15:16:27 -0400

    switch over to the more modern grid_plot

--- figures/finite-volume/fv.py
+++ figures/finite-volume/fv.py
@@ -1,5 +1,5 @@
 import math
-import numpy
+import numpy as np
 
 import matplotlib
 # Use LaTeX for rendering
@@ -7,8 +7,8 @@ matplotlib.rcParams["text.usetex"] = True
 # load the xfrac package
 matplotlib.rcParams["text.latex.preamble"].append(r'\usepackage{xfrac}')
 
-import pylab
-import grid_plot_util as gpu
+import matplotlib.pyplot as plt
+import grid_plot as gp
 
 
 
@@ -19,50 +19,51 @@ import grid_plot_util as gpu
 nzones = 8
 
 # data that lives on the grid
-#a = numpy.array([0.3, 1.0, 0.9, 0.8, 0.25, 0.15, 0.5, 0.55])
-a = numpy.array([0.3, 1.0, 0.9, 0.8, 0.25, 0.1, 0.5, 0.55])
+#a = np.array([0.3, 1.0, 0.9, 0.8, 0.25, 0.15, 0.5, 0.55])
+a = np.array([0.3, 1.0, 0.9, 0.8, 0.25, 0.1, 0.5, 0.55])
 
-gr = gpu.grid(nzones)
+gr = gp.FVGrid(nzones)
 
+pc = gp.PiecewiseConstant(gr, a)
 
-pylab.clf()
+plt.clf()
 
-gpu.drawGrid(gr)
+gr.draw_grid()
 
-gpu.labelCenter(gr, nzones/2,   r"$i$", fontsize="medium")
-gpu.labelCenter(gr, nzones/2-1, r"$i-1$", fontsize="medium")
-gpu.labelCenter(gr, nzones/2+1, r"$i+1$", fontsize="medium")
-gpu.labelCenter(gr, nzones/2-2, r"$i-2$", fontsize="medium")
-gpu.labelCenter(gr, nzones/2+2, r"$i+2$", fontsize="medium")
+gr.label_center(nzones/2,   r"$i$", fontsize="medium")
+gr.label_center(nzones/2-1, r"$i-1$", fontsize="medium")
+gr.label_center(nzones/2+1, r"$i+1$", fontsize="medium")
+gr.label_center(nzones/2-2, r"$i-2$", fontsize="medium")
+gr.label_center(nzones/2+2, r"$i+2$", fontsize="medium")
 
-gpu.labelEdge(gr, nzones/2,   r"$i-\sfrac{1}{2}$", fontsize="small")
-gpu.labelEdge(gr, nzones/2+1,   r"$i+\sfrac{1}{2}$", fontsize="small")
+gr.label_edge(nzones/2,   r"$i-\sfrac{1}{2}$", fontsize="small")
+gr.label_edge(nzones/2+1, r"$i+\sfrac{1}{2}$", fontsize="small")
 
 # draw the data
-i = 0
-while i < nzones:
-    gpu.drawCellAvg(gr, i, a[i], color="r")    
-    i += 1
+for i in range(nzones):
+    pc.draw_cell_avg(i, color="r")    
     
-gpu.labelCellAvg(gr, nzones/2, a[nzones/2], r"$\langle f\rangle_i$", color="r")
+pc.label_cell_avg(nzones/2, r"$\langle f\rangle_i$", color="r")
 
 # label dx
-pylab.plot([gr.xr[gr.ng+nzones/2-1], gr.xr[gr.ng+nzones/2-1]], [-0.35,-0.25], color="k")
-pylab.plot([gr.xr[gr.ng+nzones/2], gr.xr[gr.ng+nzones/2]], [-0.35,-0.25], color="k")
-pylab.plot([gr.xr[gr.ng+nzones/2-1], gr.xr[gr.ng+nzones/2]], [-0.3,-0.3], color="k")
-pylab.text(gr.xc[gr.ng+nzones/2], -0.45, r"$\Delta x$", 
+plt.plot([gr.xr[gr.ng+nzones/2-1], gr.xr[gr.ng+nzones/2-1]], [-0.35,-0.25], color="k")
+plt.plot([gr.xr[gr.ng+nzones/2], gr.xr[gr.ng+nzones/2]], [-0.35,-0.25], color="k")
+plt.plot([gr.xr[gr.ng+nzones/2-1], gr.xr[gr.ng+nzones/2]], [-0.3,-0.3], color="k")
+plt.text(gr.xc[gr.ng+nzones/2], -0.45, r"$\Delta x$", 
            horizontalalignment="center")
 
 
 
-pylab.axis([gr.xmin-0.5*gr.dx,gr.xmax+0.5*gr.dx, -0.5, 1.2])
-pylab.axis("off")
+gr.clean_axes()
+plt.ylim(-0.5, 1.2)
 
-pylab.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
 
-f = pylab.gcf()
+plt.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
+
+f = plt.gcf()
 f.set_size_inches(10.0,3.0)
 
-pylab.savefig("fv_grid.png")
-pylab.savefig("fv_grid.pdf")
+plt.savefig("fv_grid.png")
+plt.savefig("fv_grid.pdf")
+
 
