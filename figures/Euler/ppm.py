b7f07f659262d92e4007b26a19173adf4a011609
Michael Zingale <michael.zingale@stonybrook.edu>
2015-08-28 22:10:04 -0400

    updated to the new grid_plot module

--- figures/Euler/ppm.py
+++ figures/Euler/ppm.py
@@ -1,7 +1,7 @@
 import math
-import numpy
-import pylab
-import grid_plot_util as gpu
+import numpy as np
+import matplotlib.pyplot as plt
+import grid_plot as gp
 
 
 #-----------------------------------------------------------------------------
@@ -9,124 +9,110 @@ import grid_plot_util as gpu
 nzones = 8
 
 # data that lives on the grid
-#a = numpy.array([0.3, 1.0, 0.9, 0.8, 0.25, 0.15, 0.5, 0.55])
-a = numpy.array([0.3, 1.0, 0.9, 0.8, 0.25, 0.1, 0.5, 0.55])
+a = np.array([0.3, 1.0, 0.9, 0.8, 0.25, 0.1, 0.5, 0.55])
 
-gr = gpu.grid(nzones)
+gr = gp.FVGrid(nzones)
 
+plt.clf()
 
-pylab.clf()
+gr.draw_grid()
 
-gpu.drawGrid(gr)
-
-gpu.labelCenter(gr, nzones/2,   r"$i$")
-gpu.labelCenter(gr, nzones/2-1, r"$i-1$")
-gpu.labelCenter(gr, nzones/2+1, r"$i+1$")
-gpu.labelCenter(gr, nzones/2-2, r"$i-2$")
-gpu.labelCenter(gr, nzones/2+2, r"$i+2$")
+gr.label_center(nzones/2,   r"$i$")
+gr.label_center(nzones/2-1, r"$i-1$")
+gr.label_center(nzones/2+1, r"$i+1$")
+gr.label_center(nzones/2-2, r"$i-2$")
+gr.label_center(nzones/2+2, r"$i+2$")
 
 
-n = 0
-while (n < nzones):
-    gpu.drawCellAvg(gr, n, a[n], color="r")
-    n += 1
+pc = gp.PiecewiseConstant(gr, a)
+
+for n in range(nzones):
+    pc.draw_cell_avg(n, color="r")
 
 
-pylab.axis([gr.xmin-0.5*gr.dx,gr.xmax+0.5*gr.dx, -0.25, 1.2])
-pylab.axis("off")
+gr.clean_axes()
+plt.ylim(-0.25, 1.2)
 
-pylab.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
+plt.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
 
-f = pylab.gcf()
+f = plt.gcf()
 f.set_size_inches(8.0,2.0)
 
-pylab.savefig("piecewise-constant.eps")
-pylab.savefig("piecewise-constant.png")
+plt.savefig("piecewise-constant.pdf")
+plt.savefig("piecewise-constant.png")
 
 
 #------------- PLM -------------
-pylab.clf()
+plt.clf()
 
-gpu.drawGrid(gr)
+gr.draw_grid()
 
-gpu.labelCenter(gr, nzones/2,   r"$i$")
-gpu.labelCenter(gr, nzones/2-1, r"$i-1$")
-gpu.labelCenter(gr, nzones/2+1, r"$i+1$")
-gpu.labelCenter(gr, nzones/2-2, r"$i-2$")
-gpu.labelCenter(gr, nzones/2+2, r"$i+2$")
-
-
-n = 0
-while (n < nzones):
-    gpu.drawCellAvg(gr, n, a[n], color="0.5")
-    n += 1
-
-# compute the slopes
-da = gpu.lslopes(a, nolimit=1)
-lda = gpu.lslopes(a)
-
-n = 2
-while (n < nzones-2):
-    gpu.drawSlope(gr, n, da[n], a[n], color="r", ls=":")
-    gpu.drawSlope(gr, n, lda[n], a[n], color="r")
-    n += 1
+gr.label_center(nzones/2,   r"$i$")
+gr.label_center(nzones/2-1, r"$i-1$")
+gr.label_center(nzones/2+1, r"$i+1$")
+gr.label_center(nzones/2-2, r"$i-2$")
+gr.label_center(nzones/2+2, r"$i+2$")
 
 
-pylab.axis([gr.xmin-0.5*gr.dx,gr.xmax+0.5*gr.dx, -0.25, 1.2])
-pylab.axis("off")
+# not limited and limited
+pl_n = gp.PiecewiseLinear(gr, a, nolimit=1)
+pl_y = gp.PiecewiseLinear(gr, a)
 
-pylab.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
+for n in range(nzones):
+    pc.draw_cell_avg(n, color="0.5")
 
-f = pylab.gcf()
+for n in range(2, nzones-2):
+    pl_n.draw_slope(n, color="r", ls=":")
+    pl_y.draw_slope(n, color="r")
+
+
+gr.clean_axes()
+plt.ylim(-0.25, 1.2)
+
+plt.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
+
+f = plt.gcf()
 f.set_size_inches(8.0,2.0)
 
-pylab.savefig("piecewise-linear.eps")
-pylab.savefig("piecewise-linear.png")
+plt.savefig("piecewise-linear.pdf")
+plt.savefig("piecewise-linear.png")
 
 
 #------------- PPM -------------
-pylab.clf()
+plt.clf()
 
-gpu.drawGrid(gr)
+gr.draw_grid()
 
-gpu.labelCenter(gr, nzones/2,   r"$i$")
-gpu.labelCenter(gr, nzones/2-1, r"$i-1$")
-gpu.labelCenter(gr, nzones/2+1, r"$i+1$")
-gpu.labelCenter(gr, nzones/2-2, r"$i-2$")
-gpu.labelCenter(gr, nzones/2+2, r"$i+2$")
+gr.label_center(nzones/2,   r"$i$")
+gr.label_center(nzones/2-1, r"$i-1$")
+gr.label_center(nzones/2+1, r"$i+1$")
+gr.label_center(nzones/2-2, r"$i-2$")
+gr.label_center(nzones/2+2, r"$i+2$")
 
-
-n = 0
-while (n < nzones):
-    gpu.drawCellAvg(gr, n, a[n], color="0.5")
-    n += 1
+for n in range(nzones):
+    pc.draw_cell_avg(n, color="0.5")
 
 
-# compute the parabolic coefficients
-ap, am, a6 = gpu.ppm(a, nolimit=1)
-lap, lam, la6 = gpu.ppm(a)
+# compute the parabolic coefficients -- limited and not limited
+pm_n = gp.PiecewiseParabolic(gr, a, nolimit=1)
+pm_y = gp.PiecewiseParabolic(gr, a)
 
-n = 2
-while (n < nzones-2):
-    gpu.drawParabola(gr, n, ap[n], am[n], a6[n], color="r", ls=":")
-    gpu.drawParabola(gr, n, lap[n], lam[n], la6[n], color="r")
-    #pylab.scatter([gr.xl[n], gr.xr[n]], [am[n], ap[n]])
-    n += 1
+for n in range(2, nzones-2):
+    pm_n.draw_parabola(n, color="r", ls=":")
+    pm_y.draw_parabola(n, color="r")
 
 
+gr.clean_axes()
+plt.ylim(-0.25, 1.2)
 
+plt.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
 
-pylab.axis([gr.xmin-0.5*gr.dx,gr.xmax+0.5*gr.dx, -0.25, 1.2])
-pylab.axis("off")
-
-pylab.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
-
-f = pylab.gcf()
+f = plt.gcf()
 f.set_size_inches(8.0,2.0)
 
 
-pylab.savefig("piecewise-parabolic.eps")
-pylab.savefig("piecewise-parabolic.png")
+plt.savefig("piecewise-parabolic.pdf")
+plt.savefig("piecewise-parabolic.png")
 
                
 
