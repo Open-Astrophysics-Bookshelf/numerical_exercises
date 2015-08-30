ccb98a4d1144d2c9f914ee0b214b80bfdebee03f
Michael Zingale <michael.zingale@stonybrook.edu>
2015-08-30 14:58:17 -0400

    scaling

--- figures/Euler/ppm-sequence.py
+++ figures/Euler/ppm-sequence.py
@@ -67,136 +67,70 @@ class RiemannProblem(object):
 
 
 # left state
-q_l = riemann.State(rho=1.0, u=0.0, p=0.1)
+q_l = riemann.State(rho=1.0, u=0.0, p=1.0)
 
 # right state
-q_r = riemann.State(rho=0.1, u=0.0, p=0.125)
-
+q_r = riemann.State(rho=0.125, u=0.0, p=0.1)
 
 r = RiemannProblem(q_l, q_r)
+
+
+subidx = [311, 312, 313]
+states = ["rho", "u", "p"]
+state_labels = [r"$\rho$", r"$u$", r"$p$"]
 
 
 #-----------------------------------------------------------------------------
 #  plot 1: initial state
 plt.clf()
 
+for n, s in enumerate(states):
 
-# rho
-plt.subplot(311)
+    plt.subplot(subidx[n])
+    r.draw_grid()
+    r.draw_var_avg(s)
+    r.gr.clean_axes()
 
-r.draw_grid()
-r.draw_var_avg("rho")
-r.gr.clean_axes()
-
-plt.title(r"$\rho$")
-
-# u
-plt.subplot(312)
-
-r.draw_grid()
-r.draw_var_avg("u")
-r.gr.clean_axes()
-
-plt.title("$u$")
-
-# p
-plt.subplot(313)
-
-r.draw_grid()
-r.draw_var_avg("p")
-r.gr.clean_axes()
-
-plt.title("$p$")
+    plt.title(state_labels[n])
 
 f = plt.gcf()
 f.set_size_inches(8.0,7.0)
-
 plt.tight_layout()
 
 plt.savefig("ppm-seq-1.png")
 
 
-
-# #------------- PLM -------------
-# plt.clf()
-
-# gpu.drawGrid(gr)
-
-# gpu.labelCenter(gr, nzones/2,   r"$i$")
-# gpu.labelCenter(gr, nzones/2-1, r"$i-1$")
-# gpu.labelCenter(gr, nzones/2+1, r"$i+1$")
-# gpu.labelCenter(gr, nzones/2-2, r"$i-2$")
-# gpu.labelCenter(gr, nzones/2+2, r"$i+2$")
-
-
-# n = 0
-# while (n < nzones):
-#     gpu.drawCellAvg(gr, n, a[n], color="0.5")
-#     n += 1
-
-# # compute the slopes
-# da = gpu.lslopes(a, nolimit=1)
-# lda = gpu.lslopes(a)
-
-# n = 2
-# while (n < nzones-2):
-#     gpu.drawSlope(gr, n, da[n], a[n], color="r", ls=":")
-#     gpu.drawSlope(gr, n, lda[n], a[n], color="r")
-#     n += 1
-
-
-# plt.axis([gr.xmin-0.5*gr.dx,gr.xmax+0.5*gr.dx, -0.25, 1.2])
-# plt.axis("off")
-
-# plt.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
-
-# f = plt.gcf()
-# f.set_size_inches(8.0,2.0)
-
-# plt.savefig("piecewise-linear.eps")
-# plt.savefig("piecewise-linear.png")
-
-
-# #------------- PPM -------------
-# plt.clf()
-
-# gpu.drawGrid(gr)
-
-# gpu.labelCenter(gr, nzones/2,   r"$i$")
-# gpu.labelCenter(gr, nzones/2-1, r"$i-1$")
-# gpu.labelCenter(gr, nzones/2+1, r"$i+1$")
-# gpu.labelCenter(gr, nzones/2-2, r"$i-2$")
-# gpu.labelCenter(gr, nzones/2+2, r"$i+2$")
-
-
-# n = 0
-# while (n < nzones):
-#     gpu.drawCellAvg(gr, n, a[n], color="0.5")
-#     n += 1
-
-
-# # compute the parabolic coefficients
-# ap, am, a6 = gpu.ppm(a, nolimit=1)
-# lap, lam, la6 = gpu.ppm(a)
-
-# n = 2
-# while (n < nzones-2):
-#     gpu.drawParabola(gr, n, ap[n], am[n], a6[n], color="r", ls=":")
-#     gpu.drawParabola(gr, n, lap[n], lam[n], la6[n], color="r")
-#     #plt.scatter([gr.xl[n], gr.xr[n]], [am[n], ap[n]])
-#     n += 1
+#-----------------------------------------------------------------------------
+#  plot 2: cubic points (three vertical)
 
 
 
-
-# plt.axis([gr.xmin-0.5*gr.dx,gr.xmax+0.5*gr.dx, -0.25, 1.2])
-# plt.axis("off")
-
-# plt.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
-
-# f = plt.gcf()
-# f.set_size_inches(8.0,2.0)
+#-----------------------------------------------------------------------------
+#  plot 3: parabola (three vertical)
 
 
-# plt.savefig("piecewise-parabolic.eps")
-# plt.savefig("piecewise-parabolic.png")
+
+#-----------------------------------------------------------------------------
+#  plot 4: cell-center Riemann waves to show what hits the interface
+
+
+#-----------------------------------------------------------------------------
+#  plot 5: tracing (zoom in, 3 horizontal)
+
+
+
+#-----------------------------------------------------------------------------
+#  plot 6: final interface state (zoom in, 3 horizontal)
+
+
+
+#-----------------------------------------------------------------------------
+#  plot 7: Riemann phase
+
+
+
+#-----------------------------------------------------------------------------
+#  plot 8: Riemann solution
+
+
+
