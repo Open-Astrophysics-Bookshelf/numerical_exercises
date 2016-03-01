8e4dd2705ac6e1dc0e391673a28dd935c205c73c
Michael Zingale <michael.zingale@stonybrook.edu>
2016-02-29 20:33:00 -0500

    switch over to grid_plot

--- figures/multigrid/mgtower.py
+++ figures/multigrid/mgtower.py
@@ -1,7 +1,5 @@
-import math
-import numpy
-import pylab
-import grid_plot_util as gpu
+import matplotlib.pyplot as plt
+import grid_plot as gp
 
 # plot two stacked fv grids of different (2x) resolution to show prolongation
 
@@ -9,28 +7,25 @@ import grid_plot_util as gpu
 
 gr = []
 
-nf = 2
-while (nf <= 16):
+nzones = [2, 4, 8, 16]
+for nf in nzones:
+    gr.append(gp.FVGrid(nf, ng=1, voff=2.0*len(gr)))
 
-    gr.append(gpu.grid(nf, ng=1, voff=2.0*len(gr)))
-    nf = nf*2
 
-pylab.clf()
+plt.clf()
 
 for g in gr:
-    gpu.drawGrid(g, emphasizeEnd=1, drawGhost=1, edgeTicks=0)
+    g.draw_grid(emphasize_end=1, draw_ghost=1, edge_ticks=0)
 
-f = pylab.gcf()
+f = plt.gcf()
 f.set_size_inches(7.0,5.0)
 
 grf = gr[0]
-pylab.xlim(grf.xmin-1.1*grf.dx,grf.xmax+1.1*grf.dx)
+plt.xlim(grf.xmin-1.1*grf.dx,grf.xmax+1.1*grf.dx)
 
-pylab.axis("off")
+plt.axis("off")
 
-pylab.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
+plt.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
 
-
-pylab.savefig("mgtower.png")
-pylab.savefig("mgtower.eps")
+plt.savefig("mgtower.pdf")
 
