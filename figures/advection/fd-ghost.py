b77295c994f9836739ae5043f9343358da1bbaa3
Michael Zingale <michael.zingale@stonybrook.edu>
2016-02-29 21:08:59 -0500

    switch to grid_plot

--- figures/advection/fd-ghost.py
+++ figures/advection/fd-ghost.py
@@ -1,7 +1,6 @@
-import math
-import numpy
-import pylab
-import grid_plot_util as gpu
+import numpy as np
+import matplotlib.pyplot as plt
+import grid_plot as gp
 
 # plot a simple finite-difference grid
 
@@ -10,53 +9,45 @@ import grid_plot_util as gpu
 nzones = 9
 
 # data that lives on the grid
-#a = numpy.array([0.3, 1.0, 0.9, 0.8, 0.25, 0.15, 0.5, 0.55])
-a = numpy.array([0.55, 0.3, 1.0, 0.9, 0.8, 0.25, 0.1, 0.5, 0.55])
+#a = np.array([0.3, 1.0, 0.9, 0.8, 0.25, 0.15, 0.5, 0.55])
+a = np.array([0.55, 0.3, 1.0, 0.9, 0.8, 0.25, 0.1, 0.5, 0.55])
 
-gr = gpu.grid(nzones, ng=1, fd=1)
+gr = gp.FDGrid(nzones, ng=1)
 
+plt.clf()
 
-pylab.clf()
-
-gpu.drawGrid(gr, drawGhost=1)
+gr.draw_grid(draw_ghost=1)
 
 labels = ["-1", "0", "1", "", "i-1", "i", "i+1", "", "N-2", "N-1", "N"]
 
-i = gr.ilo-gr.ng
-while (i < gr.ng+gr.nx+1):
-
+for i in range(gr.ilo-gr.ng, gr.ng+gr.nx+1):
     if not labels[i] == "":
-        gpu.labelCenter(gr, i,   r"$%s$" % (labels[i]), fontsize="medium")
-    i += 1
-
+        gr.label_node(i, r"$%s$" % (labels[i]), fontsize="medium")
     
 # draw the data
-i = gr.ilo
-while i < gr.ihi+1:
-    gpu.drawFDData(gr, i, a[i-gr.ng], color="r")    
-    i += 1
+for i in range(gr.ilo, gr.ihi+1):
+    gr.draw_data(i, a[i-gr.ng], color="r")    
     
 
-gpu.labelFD(gr, gr.ilo+4, a[gr.ilo+4-gr.ng], r"$a_i$", color="r")
+gr.label_value(gr.ilo+4, a[gr.ilo+4-gr.ng], r"$a_i$", color="r")
 
 # label dx
-pylab.plot([gr.xc[gr.ng+nzones/2-1], gr.xc[gr.ng+nzones/2-1]], [-0.35,-0.25], color="k")
-pylab.plot([gr.xc[gr.ng+nzones/2], gr.xc[gr.ng+nzones/2]], [-0.35,-0.25], color="k")
-pylab.plot([gr.xc[gr.ng+nzones/2-1], gr.xc[gr.ng+nzones/2]], [-0.3,-0.3], color="k")
-pylab.text(0.5*(gr.xc[gr.ng+nzones/2-1] + gr.xc[gr.ng+nzones/2]), -0.45, 
+plt.plot([gr.xc[gr.ng+nzones/2-1], gr.xc[gr.ng+nzones/2-1]], [-0.35,-0.25], color="k")
+plt.plot([gr.xc[gr.ng+nzones/2], gr.xc[gr.ng+nzones/2]], [-0.35,-0.25], color="k")
+plt.plot([gr.xc[gr.ng+nzones/2-1], gr.xc[gr.ng+nzones/2]], [-0.3,-0.3], color="k")
+plt.text(0.5*(gr.xc[gr.ng+nzones/2-1] + gr.xc[gr.ng+nzones/2]), -0.45, 
            r"$\Delta x$", 
            horizontalalignment="center", fontsize=16)
 
 
-pylab.axis([gr.xmin-1.1*gr.dx,gr.xmax+1.1*gr.dx, -0.5, 1.3])
-pylab.axis("off")
+plt.axis([gr.xmin-1.1*gr.dx,gr.xmax+1.1*gr.dx, -0.5, 1.3])
+plt.axis("off")
 
-pylab.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
+plt.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.95)
 
-f = pylab.gcf()
+f = plt.gcf()
 f.set_size_inches(10.0,3.0)
 
 
-pylab.savefig("fd_ghost.png")
-pylab.savefig("fd_ghost.eps")
+plt.savefig("fd_ghost.pdf")
 
